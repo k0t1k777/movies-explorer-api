ae104b81af8499774d4ed1146807d36318eeb7a6
Nickolai <lindeman072@mail.ru>
2023-09-16 12:58:08 +0400

    исправления роутов удаления фильмов #2

--- controllers/movies.js
+++ controllers/movies.js
@@ -47,14 +47,26 @@ module.exports.getMovie = (req, res, next) => {
     .catch(next);
 };
 
+// module.exports.deleteMovie = (req, res, next) => {
+//   const { movieId } = req.params;
+//   Movie.findById(movieId)
+//     .orFail(new NotFoundError('Фильм с таким id не найден'))
+//     .then((movie) => {
+//       if (!movie.owner.equals(req.user._id)) {
+//         return Promise.reject(new ForbiddenError('Нельзя удалять чужие фильмы'));
+//       }
+//       return Movie.deleteOne(movie)
+//         .then(() => res.send({ data: 'Удаление прошло успешно' }));
+//     })
+//     .catch(next);
+// };
+
 module.exports.deleteMovie = (req, res, next) => {
   const { movieId } = req.params;
   Movie.findById(movieId)
     .orFail(new NotFoundError('Фильм с таким id не найден'))
     .then((movie) => {
-      if (!movie.owner.equals(req.user._id)) {
-        return Promise.reject(new ForbiddenError('Нельзя удалять чужие фильмы'));
-      }
+      if (movie.owner.toString() !== req.user._id) return next(new ForbiddenError('Нельзя удалять чужие фильмы'));
       return Movie.deleteOne(movie)
         .then(() => res.send({ data: 'Удаление прошло успешно' }));
     })
